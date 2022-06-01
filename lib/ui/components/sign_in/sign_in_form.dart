149b2db64d4b2bd7b8950732d618f9e7cd234458
t-nakano <superhoneycomb@gmail.com>
2022-06-01 20:41:06 +0900

    modify sign_in_screen

---
+++ lib/ui/components/sign_in/sign_in_form.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:warikan_app/data/consts/texts.dart';
+import 'package:warikan_app/ui/components/common/input_field.dart';
+import 'package:warikan_app/ui/components/common/input_label.dart';
+import 'package:warikan_app/ui/components/common/wide_button.dart';
+import 'package:warikan_app/ui/viewmodels/sign_in_viewmodel.dart';
+
+class SignInForm extends StatelessWidget {
+  const SignInForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _globalKey = GlobalKey<FormState>();
+    final vm = context.read<SignInViewModel>();
+
+    return ConstrainedBox(
+      constraints: const BoxConstraints(minHeight: 300),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height / 3,
+        child: Form(
+          key: _globalKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const InputLabel(label: FormLabels.email),
+              InputField(
+                onChanged: vm.inputEmail,
+                validator: vm.emailValidator,
+              ),
+              const InputLabel(label: FormLabels.pass),
+              InputField(
+                onChanged: vm.inputPass,
+                validator: vm.passValidator,
+              ),
+              WideButton(
+                text: ButtonLabels.signIn,
+                onPressed: () => vm.executeValidator(context, _globalKey),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
