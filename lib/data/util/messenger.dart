01ff51ed38c198d192514903413f6365f492bc00
RyomaArisawa <ryoma.arisawa@isub.co.jp>
2022-06-15 21:44:51 +0900

    modified error message

---
+++ lib/data/util/messenger.dart
@@ -0,0 +1,21 @@
+import 'package:warikan_app/data/consts/error_messages.dart';
+
+class Messenger {
+  static String authErrorMsg(String error) {
+    String errorMsg;
+    if (error.toString().contains("wrong-password")) {
+      //パスワードが間違えていた場合
+      errorMsg = AuthError.wrongPass;
+    } else if (error.toString().contains("user-not-found")) {
+      //ユーザーが登録されていない場合
+      errorMsg = AuthError.notFound;
+    } else if (error.toString().contains("email-already-in-use")) {
+      //すでに同じメールアドレスでユーザが登録されている場合
+      errorMsg = AuthError.alreadyUsed;
+    } else {
+      //その他エラー
+      errorMsg = AuthError.other;
+    }
+    return errorMsg;
+  }
+}
